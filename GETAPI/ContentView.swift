da76f2df2df244c0f4b991daed93ece9e9b22dd2
mux02 <moomksa2015@gmail.com>
2022-07-24 05:26:52 +0300

    Initial Commit

---
+++ GETAPI/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GETAPI
+//
+//  Created by Mohammed Almalki on 25/12/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GETAPI/GETAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GETAPIApp.swift
+//  GETAPI
+//
+//  Created by Mohammed Almalki on 25/12/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct GETAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
